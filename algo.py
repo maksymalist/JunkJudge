0d3e0c580ab55727a20ea617fafebc3ef70fdd4b
John Doe <79988159+John8790909@users.noreply.github.com>
2023-05-04 19:37:29 -0400

    untracking images

--- algo.py
+++ algo.py
@@ -3,7 +3,7 @@ from utils import CLASSES_1, CLASSES_2
 
 def final_say(out1, out2, out3):
 
-    # add your logic here
+    # add your logic here 
     
     return Counter([out1, out2, out3]).most_common(1)[0][0]
     
