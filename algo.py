8619bd6f30a84facd92441bbb316e054f9f85383
John Doe <79988159+John8790909@users.noreply.github.com>
2023-05-10 23:35:02 -0400

    increased accuracy to 95%

--- algo.py
+++ algo.py
@@ -20,12 +20,12 @@ def final_say(v1, v2, out1, out2, out3, probas):
     simularities = torch.tensor(simularities).to(DEVICE)
     print(f"confidence {out3}: ", simularities.mean())
         
-    
+    # makes trash predictions more confident
     if "trash" in v2:
         trash_confidence = v2["trash"] / sum(v2.values())
         verdict_confidence = v1[max(v1, key=v1.get)] / sum(v1.values())
         
-        if trash_confidence > verdict_confidence:
+        if trash_confidence > verdict_confidence and verdict_confidence < 0.65:
             return "trash"
     
     return Counter([out1, out2, out3]).most_common(1)[0][0]
