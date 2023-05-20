a71a7bcf92647309b42a7cbaba464905b869b45b
John Doe <79988159+John8790909@users.noreply.github.com>
2023-05-20 08:16:55 -0400

    added build file

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt update
+
+# Install required packages
+sudo apt install -y wget zip unzip gunicorn python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+
+# Clone the repository
+git clone https://github.com/maksymalist/JunkJudge.git
+
+# Navigate into the cloned repository
+cd JunkJudge
+
+# Install virtualenv
+sudo apt install python3-venv
+
+python3 -m venv .venv
+
+source .venv/bin/activate
+
+pip3 install -r requirements.txt
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+
+wget https://github.com/maksymalist/JunkJudge/releases/download/v1.0/models.zip -O models.zip && \
+    unzip -d models.zip && \
+    rm models.zip
+
+sudo ufw allow 5000
+
+gunicorn --bind 0.0.0.0:5000 wsgi:app
