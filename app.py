b85776264b6a1bd339b41d1017165be08ef5eac7
maksymalist <maksymvovkk@gmail.com>
2023-07-04 18:57:41 -0400

    cleaned up app.py

--- app.py
+++ app.py
@@ -50,23 +50,3 @@ def predict():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
-
-# VISUALIZATION
-
-# fig, ax = plt.subplots(1, 3, figsize=(10, 10))
-
-# ax[0].pie(v1.values(), labels=v1.keys())
-# ax[0].margins(x=20, y=10)
-# ax[0].set_title("First Model")
-
-# ax[1].pie(v2.values(), labels=v2.keys())
-# ax[1].margins(x=20, y=10)
-# ax[1].set_title("Second Model")
-
-# ax[2].imshow(img1)
-# ax[2].axis("off")
-# ax[2].margins(x=20, y=10)
-# ax[2].set_title(final_verdict)
-
-# plt.show()
